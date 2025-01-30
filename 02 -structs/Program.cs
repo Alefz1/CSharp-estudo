16cb46b4e3323575d1e9cb1c91c1cfe84325927e
Alefz1 <alefemanoel96@gmail.com>
2025-01-30 15:25:24 -0300

    commit

---
+++ 02 -structs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+   static void Main(string[] args)
+   {
+    List<string> clientes = new List<string>();
+
+    clientes.Add("Alef");
+    clientes.Add("Emilly");
+
+    clientes.RemoveAt(0);
+
+
+    foreach (string cliente in clientes)
+    {
+        Console.WriteLine(cliente);
+    }
+
+    Console.WriteLine(clientes.IndexOf("Emilly"));
+   }
+}
